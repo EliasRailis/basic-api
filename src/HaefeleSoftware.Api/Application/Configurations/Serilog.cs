2871bd655bc980dd1219df62f5fed7ef24b8ad1c
Elias Railis <elias.railis97@gmail.com>
2024-06-14 22:54:51 +0200

    added serilog as the logging provider of the application

---
+++ src/HaefeleSoftware.Api/Application/Configurations/Serilog.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace HaefeleSoftware.Api.Application.Configurations;
+
+public static class Serilog
+{
+    public static void AddSerilog(this IServiceCollection service)
+    {
+        var serilogFile = new ConfigurationBuilder()
+            .AddJsonFile("serilog.setup.json")
+            .Build();
+        
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(serilogFile)
+            .CreateLogger();
+
+        service.AddSingleton(Log.Logger);
+        service.AddLogging(loggingBuilder =>
+        {
+            loggingBuilder.ClearProviders();
+            loggingBuilder.AddSerilog(dispose: true);
+        });
+    }
+}
