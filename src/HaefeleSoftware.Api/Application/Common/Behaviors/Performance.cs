6b4cc46c509a53af73b55466ac90dbb05bb20fe8
Elias Railis <elias.railis97@gmail.com>
2024-06-15 00:00:07 +0200

    performance behavior

---
+++ src/HaefeleSoftware.Api/Application/Common/Behaviors/Performance.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using MediatR;
+using Serilog;
+
+namespace HaefeleSoftware.Api.Application.Common.Behaviors;
+
+public sealed class Performance<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> 
+    where TRequest : IRequest<TResponse>
+{
+    private readonly Stopwatch _timer;
+    private readonly ILogger _logger;
+
+    public Performance(ILogger logger)
+    {
+        _timer = new Stopwatch();
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, 
+        CancellationToken cancellationToken)
+    {
+        _timer.Start();
+        var response = await next();
+        _timer.Stop();
+        
+        var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+        if (elapsedMilliseconds <= 500) return response;
+        
+        var requestName = typeof(TRequest).Name;
+        _logger.Warning("Long running request {RequestName} ({ElapsedMilliseconds} milliseconds)", 
+            requestName, elapsedMilliseconds);
+        
+        return response;
+
+    }
+}
