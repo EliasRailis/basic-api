56e17b6eec3ce137c46edf052b3a663d7eb36456
Elias Railis <elias.railis97@gmail.com>
2024-06-15 00:13:59 +0200

    added the audit class

---
+++ src/HaefeleSoftware.Api/Domain/Common/Audit.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HaefeleSoftware.Api.Domain.Common;
+
+public class Audit
+{
+    [Column(Order = 1)]
+    public required DateTime Created { get; set; }
+    
+    [Column(Order = 2)]
+    public required string CreatedBy { get; set; } 
+    
+    [Column(Order = 3)]
+    public DateTime? LastModified { get; set; }
+    
+    [Column(Order = 4)]
+    public string? LastModifiedBy { get; set; }
+}
